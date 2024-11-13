06937639acbb0d0994f52b4a8ae60f3298d82be9
Shahad842 <shahad_9215@hotmail.com>
2024-11-13 10:56:36 +0300

    Initial Commit

---
+++ Khafoot2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Khafoot2
+//
+//  Created by Shahad Alhothali on 11/05/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Khafoot2/Khafoot2App.swift
@@ -0,0 +1,17 @@
+//
+//  Khafoot2App.swift
+//  Khafoot2
+//
+//  Created by Shahad Alhothali on 11/05/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Khafoot2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
